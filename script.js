79d5e970bb89d46341b5e5202e63a211c76055dc
MatthewEspayos <>
2021-06-08 13:40:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Creating variable to store numbers
+let randomnumber = 0
+let userguess = 0
+// Connect "Guess" button to "random" function
+document.getElementById('mybutton').addEventListener('click', randomizer)
+// Get number typed in by user and convert it to integer
+function randomizer () {
+  userguess = document.getElementById('myinput').value
+  userguess = parseInt(userguess)
+  // Generate random number between 1 and 6
+  randomnumber = (Math.random() * 6) + 1
+  randomnumber = parseInt(randomnumber)
+  // Compares user guess with random number
+  if (userguess == randomnumber) {
+    document.getElementById('myanswer').innerHTML = 'You win!'
+  }
+}
